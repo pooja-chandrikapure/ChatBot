fc57a85031d97b7bf981db26260ac11ae3427baf
Pooja Chandrikapure <30poojachandrikapure@gmail.com>
2026-01-28 17:40:50 +0530

    main loyout changes

--- src/views/Layout/Main-Layout.vue
+++ src/views/Layout/Main-Layout.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex min-h-screen bg-gray-100">
+  <div class="flex  bg-gray-100">
     <Sidebar />
 
-    <main class="flex-1 p-6">
+    <main class="flex-1">
       <router-view />
     </main>
   </div>
