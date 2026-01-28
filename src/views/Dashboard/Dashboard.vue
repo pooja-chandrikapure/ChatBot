586358ab7a147d0c3dea89f8522733d200f4896a
Pooja Chandrikapure <30poojachandrikapure@gmail.com>
2026-01-28 17:39:44 +0530

    dashboard chnafges

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -1,40 +1,20 @@
 <template>
-  <div class="max-w-lg mx-auto mt-10 p-6 bg-white rounded-xl shadow">
-
-    <h2 class="text-2xl font-semibold mb-4">Welcome</h2>
-
-    <!-- Loading -->
-    <p v-if="loading" class="text-gray-500">
-      Loading profile...
-    </p>
-
-    <!-- Error -->
-    <p v-else-if="error" class="text-red-500">
-      {{ error }}
-    </p>
-
-    <!-- Profile -->
-    <div v-else-if="profile" class="space-y-3 font-semibold">
-      
-      <p><strong></strong> {{ profile.name }}</p>
-     
-     
-
+  <!-- <div class="p-4 md:p-6">
+    <div class="bg-white rounded-lg shadow p-4 md:p-6 lg:p-8">
+      <p class="text-sm md:text-base lg:text-lg text-gray-700">
+        Welcome to your dashboard 🎉
+      </p>
     </div>
-
-  </div>
+  </div> -->
+  <!-- <div
+  @click="router.push('/home')"
+  >
+    
+  </div> -->
 </template>
-
 <script setup>
-import Sidebar from '../../components/Sidebar.vue';
-import { onMounted } from 'vue';
-import { storeToRefs } from 'pinia';
-import { useAuthStore } from '../../stores/auth';
-
-const profileStore = useAuthStore();
-const { profile, loading, error, message } = storeToRefs(profileStore);
-
-onMounted(() => {
-  profileStore.fetchProfile();
-});
+import { useRoute } from 'vue-router';
+import router from '../../router';
+const route = useRoute();
+router.push('/home');
 </script>
