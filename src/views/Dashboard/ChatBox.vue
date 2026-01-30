f36bc37c4707a23249176a7489385e89ee1fff6f
Pooja Chandrikapure <30poojachandrikapure@gmail.com>
2026-01-30 18:16:55 +0530

    chatBox

--- src/views/Dashboard/ChatBox.vue
+++ src/views/Dashboard/ChatBox.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col h-[100dvh] bg-gray-50">
+  <div class="flex flex-col min-h-screen bg-gray-50">
     <!-- Header -->
     <div class="p-2 sm:p-1.5 border-b bg-white flex items-center gap-3 shrink-0">
       <button
@@ -16,7 +16,7 @@
       </div>
       <div>
         <p class="font-bold">{{ chat.strategy_name }}</p>
-        <p class="font-normal">{{ chat.creator_name }}</p>
+        <p class="font-normal">{{ getChatDisplayName(chat) }}</p>
         <!-- <p class="text-xs text-gray-500">Chat ID {{ chat.chat_id }}</p> -->
       </div>
     </div>
@@ -40,9 +40,31 @@
       <div
       class="max-w-[80%] sm:max-w-xs px-3 py-2 rounded-lg text-sm break-words shadow "
       :class="Number(msg.sender_id) === currentUserId
-      ? 'bg-green-500 text-white rounded-br-none'
+      ? 'bg-green-200 text-black rounded-br-none'
       : 'bg-gray-200 text-black rounded-bl-none'">
       {{ msg.content }} 
+      <!-- 🔥 READ STATUS (ONLY FOR MY MESSAGES) -->
+      <div
+        v-if="Number(msg.sender_id) === currentUserId"
+        class="text-[11px] text-right mt-1 flex justify-end"
+      >
+        <span
+          v-if="msg.is_read"
+          class="text-blue-400 font-semibold"
+        >
+        <img src="/tick-blue.svg  " alt=""
+          class="w-10 h-5 border-black"/>
+          <!-- ✓✓ -->
+        </span>
+        <span
+          v-else
+          class="text-gray/80"
+        >
+        <img src="/tick-black.svg  " alt=""
+          class="w-10 h-5 border-black"/>
+          <!-- ✓ -->
+        </span>
+      </div>
       </div>
       </div>
     </div>
@@ -57,10 +79,35 @@
         focus:outline-none focus:ring-1 focus:ring-blue-400"
       />
       <button
-      @click="handleSend"
-      class="bg-green-500 text-white px-4 py-2 rounded-full">
-        Send
-      </button>
+  @click="handleSend"
+  :disabled="loading"
+  class="bg-green-500 text-white px-4 py-2 rounded-full flex items-center justify-center min-w-[70px]
+         disabled:opacity-60 disabled:cursor-not-allowed"
+>
+  <span v-if="!loading">Send</span>
+
+  <svg
+    v-else
+    class="animate-spin h-5 w-5 text-white"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <circle
+      class="opacity-25"
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="currentColor"
+      stroke-width="4"
+    />
+    <path
+      class="opacity-75"
+      fill="currentColor"
+      d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+    />
+  </svg>
+</button>
     </div>
   </div>
 </template>
@@ -88,13 +135,18 @@ const {lastMessage} = storeToRefs(socketStore)
 const messageText = ref('')
 
 const loading = ref(false);
-const chatId = 5;
+const chatId = computed(() => props.chat.chat_id);
 const role = "user"; //or "creator"
 const currentUserId = computed(() => Number(authStore.profile?.id));
-
-onMounted(() => {
-  socketStore.initChat({ chat_id: chatId, role: "user"}); //role: props.chat.role
-});
+const myId = computed(() => Number(authStore.profile?.id));
+// onMounted(() => {
+//   socketStore.initChat({ chat_id: chatId.value, role: "user"}); 
+// });
+const getChatDisplayName = (chat) => {
+  return Number(chat.creator_id) === myId.value
+    ? chat.user_name
+    : chat.creator_name
+}
 
 onUnmounted(() => {
   socketStore.leaveChat();
@@ -104,7 +156,10 @@ const chatBody = ref(null);
 
 const scrollToBottom = () => {
   if (chatBody.value) {
-    chatBody.value.scrollTop = chatBody.value.scrollHeight;
+    chatBody.value.scrollTo({
+      top: chatBody.value.scrollHeight,
+      behavior: 'smooth'
+    })
   }
 };
 // onUnmounted(() => {
@@ -120,7 +175,7 @@ watch(lastMessage , () => {
 })
 
 watch(
-  () => props.chat.chat_id,
+  () => props.chat.id,
   async (id) => {
     if (id) {
       await msgStore.fetchMessages(id)
@@ -132,7 +187,7 @@ watch(
 
 
 const handleSend = async () => {
-  if (!messageText.value.trim()) return
+  if (!messageText.value.trim() || loading.value) return
   // if (!messageInput.value.trim()) return;
 
   loading.value = true;
@@ -141,14 +196,20 @@ const handleSend = async () => {
   
   // messageInput.value = "";
   
-  await msgStore.sendMessage(
-    props.chat.chat_id,
-    messageText.value
-  )
+  
+  try {
+    await msgStore.sendMessage(
+      props.chat.id,
+      messageText.value
+    )
 
-  messageText.value = '';
-
-  nextTick(scrollToBottom)
+    messageText.value = ''
+    nextTick(scrollToBottom)
+  } catch (err) {
+    console.error('Send failed', err)
+  } finally {
+    loading.value = false
+  }
 }
 
 </script>
